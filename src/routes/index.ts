d8c2db533fac3cce91211491e86755d0fe631bbb
Camilo Henao Navarro <camilohenao@MacBook-Pro-de-Camilo.local>
2021-03-06 08:27:08 -0500

    feat: add graphql config

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import testRouter from './test.routes';
+import graphqlRouter from './graphql.routes';
 
 const routes = Router();
 
 routes.use('/test', testRouter);
+routes.use('/graphql', graphqlRouter);
 
 export default routes;
